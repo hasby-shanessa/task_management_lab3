45ae36f7ad243607e21247cc9e086e7595bdc5a5
hasby-shanessa <umutoniwabohasby@gmail.com>
2025-11-26 19:41:36 +0200

    creating hardware project

--- Services/ProjectService.java
+++ Services/ProjectService.java
@@ -1,5 +1,6 @@
 package Services;
 
+import Models.HardwareProject;
 import Models.Project;
 import Models.SoftwareProject;
 
@@ -16,12 +17,24 @@ public class ProjectService {
     }
 
     //Create software project
-    public SoftwareProject createSoftwareProject(String name, String projectDescription, int teamSize, String budget, String language, String framework, String repositoryUrl){
+    public SoftwareProject createSoftwareProject(String projectName, String projectDescription, int teamSize, String budget, String language, String framework, String repositoryUrl){
         if(projectCount >= MAX_PROJECTS){
             System.out.println("Error: Maximum projects reached (" + MAX_PROJECTS + ")");
             return null;
         }
-        SoftwareProject project = new SoftwareProject(name, projectDescription, teamSize, budget, language, framework, repositoryUrl);
+        SoftwareProject project = new SoftwareProject(projectName, projectDescription, teamSize, budget, language, framework, repositoryUrl);
+        projects[projectCount] = project;
+        projectCount++;
+        return project;
+    }
+
+    //Create hardware project
+    public HardwareProject createHardwareProject(String projectName, String projectDescription, int teamSize, String budget, String components, String supplier){
+        if(projectCount >= MAX_PROJECTS){
+            System.out.println("Error: Maximum projects reached (" + MAX_PROJECTS + ")");
+            return null;
+        }
+        HardwareProject project = new HardwareProject(projectName, projectDescription, teamSize, budget, components, supplier);
         projects[projectCount] = project;
         projectCount++;
         return project;
