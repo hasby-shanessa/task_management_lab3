00b82c3a5685d972ade6785852b6b7684995e075
hasby-shanessa <umutoniwabohasby@gmail.com>
2025-11-29 17:56:16 +0200

    generate status report

--- Services/ReportService.java
+++ Services/ReportService.java
@@ -1,4 +1,20 @@
 package Services;
 
+import Models.StatusReport;
+
 public class ReportService {
+    private ProjectService projectService;
+    public ReportService(ProjectService projectService){
+        this.projectService = projectService;
+    }
+    //generate status report
+public StatusReport generateStatusReport(){
+        StatusReport report = new StatusReport();
+        report.setTotalProjects(projectService.getProjectCount());
+        report.setCompletedProjects(projectService.getCompletedProjectCount());
+        report.setTotalTasks(projectService.getTotalTaskCount());
+        report.setCompletedTasks(projectService.getCompletedTaskCount());
+        report.setAverageCompletion(projectService.getAverageCompletion());
+        return report;
+    }
 }
