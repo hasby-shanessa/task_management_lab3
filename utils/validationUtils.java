41fc40dc6a374c5fbb5ccff92495f853cc40d7d3
hasby-shanessa <umutoniwabohasby@gmail.com>
2025-11-30 00:08:11 +0200

    read task id

--- utils/validationUtils.java
+++ utils/validationUtils.java
@@ -5,52 +5,52 @@ import java.util.Scanner;
 public class validationUtils {
     private static Scanner scanner = new Scanner(System.in);
 
-    //read non empty string
-    public static String readNonEmptyString(String prompt){
+    //read nonempty string
+    public static String readNonEmptyString(String prompt) {
         String input;
-        while (true){
-            System.out.println(prompt);
+        while (true) {
+            System.out.print(prompt);
             input = scanner.nextLine().trim();
-            if(input.isEmpty()){
+            if (input.isEmpty()) {
                 System.out.println("Error: Input cannot be empty, please try again");
-            } else{
+            } else {
                 return input;
             }
         }
     }
 
     //ready any string
-    public static String readStrin(String prompt){
+    public static String readStrin(String prompt) {
         System.out.println(prompt);
         return scanner.nextLine().trim();
     }
 
     //read integer
-    public static int readInt(String prompt){
-        while(true){
+    public static int readInt(String prompt) {
+        while (true) {
             try {
-                System.out.println(prompt);
+                System.out.print(prompt);
                 String input = scanner.nextLine().trim();
                 return Integer.parseInt(input);
-            } catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Error: Please enter a valid number");
             }
         }
     }
 
     //read integer in range(between min and max)
-    public static int readIntInRange(String prompt, int min, int max){
-        while(true){
-            try{
-                System.out.println(prompt);
+    public static int readIntInRange(String prompt, int min, int max) {
+        while (true) {
+            try {
+                System.out.print(prompt);
                 String input = scanner.nextLine().trim();
                 int value = Integer.parseInt(input);
-                if(value >=min&& value<=max){
+                if (value >= min && value <= max) {
                     return value;
                 } else {
                     System.out.println("Please enter a number between " + min + " and " + max);
                 }
-            } catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Error: Please enter a valid number");
             }
         }
@@ -76,23 +76,24 @@ public class validationUtils {
     }
 
     //read valid task status
-    public static String readTaskStatus(String prompt){
-        while(true){
+    public static String readTaskStatus(String prompt) {
+        while (true) {
             System.out.print(prompt);
             String input = scanner.nextLine().trim();
-            if(input.equalsIgnoreCase("Pending") || input.equals("1")){
+            if (input.equalsIgnoreCase("Pending") || input.equals("1")) {
                 return "Pending";
-            } else if(input.equalsIgnoreCase("In Progress") || input.equalsIgnoreCase("InProress") || input.equals("2")){
+            } else if (input.equalsIgnoreCase("In Progress") || input.equalsIgnoreCase("InProress") || input.equals("2")) {
                 return "In Progress";
-            } else if(input.equalsIgnoreCase("Completed") || input.equalsIgnoreCase("Complete") || input.equalsIgnoreCase("Done") || input.equals("3")){
+            } else if (input.equalsIgnoreCase("Completed") || input.equalsIgnoreCase("Complete") || input.equalsIgnoreCase("Done") || input.equals("3")) {
                 return "Completed";
             } else {
                 System.out.println("Error: Invalid status, please choose from (Pending, In progress, Completed)");
             }
         }
     }
+
     //display status options
-    public static void displayStatusOptions(){
+    public static void displayStatusOptions() {
         System.out.println("Status Options:");
         System.out.println("1. Pending");
         System.out.println("2. In Progress");
@@ -100,21 +101,42 @@ public class validationUtils {
     }
 
     //read project id
-    public static String readProjectId(String prompt){
-        while(true){
-            System.out.println(prompt);
+    public static String readProjectId(String prompt) {
+        while (true) {
+            System.out.print(prompt);
             String input = scanner.nextLine().trim().toUpperCase();
 
-            if(input.matches("P\\D{3}")){
+            if (input.matches("P\\D{3}")) {
                 return input;
             }
-            if(input.matches("\\d+")){
-                try{
+            if (input.matches("\\d+")) {
+                try {
                     int num = Integer.parseInt(input);
                     return "P" + String.format("%03d", num);
-                } catch (NumberFormatException e){}
+                } catch (NumberFormatException e) {
+                }
             }
             System.out.println("Error: Invalid input, please enter a valid numeric or prefixed ID(e.g. P001");
+        }
+    }
+
+    //read task id
+    public static String readTaskId(String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            String input = scanner.nextLine().trim().toUpperCase();
+
+            if (input.matches("T\\d{3}")) {
+                return input;
+            }
+            if (input.matches("\\d+")) {
+                try {
+                    int num = Integer.parseInt(input);
+                    return "T" + String.format("%03d", num);
+                } catch (NumberFormatException e) {
+                }
+            }
+            System.out.println("Error: Invalid input, please enter a valid Task ID(e.g. T001");
         }
     }
 }
